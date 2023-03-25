94a6af3421b6b6ecd87abe3c8590451b64a9e552
nawriz <nawriznukusbaev@gmail.com>
2023-03-25 16:21:37 +0500

    final project

--- src/pages/task/task1.js
+++ src/pages/task/task1.js
@@ -1,9 +1,13 @@
 import * as React from "react";
 import {useState} from "react";
-
+import {motion,AnimatePresence} from "framer-motion";
 export const Task1 = () => {
-
-
+    const [isVisible, setVisible] = useState(false);
+    const [isVisible2, setVisible2] = useState(false);
+    const handleVisibality = () =>{ setVisible(!isVisible)}
+    const handleVisibality2 = () =>{ setVisible2(!isVisible)}
+    setTimeout(handleVisibality,0);
+    setTimeout(handleVisibality2,0);
     return (<div className="container-xl flex flex w-[100%] h-[100vh] justify-center items-center">
                 <div className="flex flex-col items-center">
                     <p className="m-[50px] text-3xl">25 gr natriy oksidinin’  (Na<sub>2</sub>O)  mug’darin tabin’?</p>
@@ -16,11 +20,31 @@ export const Task1 = () => {
                       <div className="flex flex-col justify-center items-center rounded-lg shadow-inner bg-slate-50 w-[500px] h-[350px] p-[20px] m-[20px]">
                           <div className="flex flex-row justify-center items-center mb-[50px]">
                               <div className="flex flex-col ml-[50px] h-[120px] w-[50px]  justify-between">
-                                  <p className="text-2xl ">160</p>
+                                  <AnimatePresence>
+                                      {
+                                          isVisible && (<motion.p
+                                              initial={{opacity:0}}
+                                              animate={{opacity:1}}
+                                              transition={{duration:3}}
+                                              className="text-2xl ">
+                                              160
+                                          </motion.p>)
+                                      }
+                                  </AnimatePresence>
+
                                   <p className="text-2xl">112</p>
                               </div>
                               <div className="m-[10px]">
-                                  <div className="w-[200px] h-[5px] rotate-[25deg] bg-[black]"></div>
+                                  <AnimatePresence>
+                                      {
+                                          isVisible2 &&(<motion.div
+                                              initial={{height:"5px",width:'0px', opacity:0}}
+                                              animate={{height:'5px',width:'200px',opacity:1}}
+                                              transition={{duration:5}}
+                                              className="bg-[black]"></motion.div>)
+                                      }
+
+                                      </AnimatePresence>
                                   <div className="absolute z-[100]  w-[200px] h-[5px] rotate-[-25deg] bg-[black]"></div>
                               </div>
                               <div className="flex flex-col mr-[50px] h-[120px] w-[50px] justify-between items-center">
