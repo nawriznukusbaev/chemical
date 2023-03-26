add870d4ed407d02d1bf6951c4a4f3fae4182a93
Nawriz Nukusbaev <nawriznukusbaev@gmail.com>
2023-03-26 23:55:18 +0500

    Demo project

--- src/datas.js
+++ src/datas.js
@@ -1,16 +1,510 @@
 export const datas = [
     {
         id: 1,
-        name: 'g63',
-        img: '../img/1.jpg',
-        price: 50000,
-        count: 1
+        task: `25 gr natriy oksidinin’  (Na<sup>2</sup>O)  mug’darin tabin’?`,
+        desc:`Berilgen: Mr(Na<sup>2O</sup>) =2*23+16=62gr/mol`,
+        a: `1mol`,
+        b: `62gr`,
+        c: `x`,
+        d: `25`,
+        x:`0,4mol`
     },
     {
-        id:2,
-        name: 'g62',
-        img: '../img/2.jpg',
-        price: 100000,
-        count: 1
-    }
+        id: 2,
+        task: `Massasi 12 gr bolg’an ku’kirt (IV) oksidindegi zat mug’darin tabin’?`,
+        desc:`Berilgen:Mr(SO<sup>2</sup>)=32+16*2=64gr/mol`,
+        a: `1mol`,
+        b: `64gr`,
+        c: `x`,
+        d: `12`,
+        x:`0,187mol`
+    },
+    {
+        id: 3,
+        task: `Massasi 25,5gr bolg’an Al<sup>2</sup>O<sup>3</sup> mug’darin  tabin’?`,
+        desc:`Berilgen: Mr(Al<sup>2</sup>O<sup>3</sup>)=27*2+16*3=102gr/mol`,
+        a: `1mol`,
+        b: `102gr`,
+        c: `x`,
+        d: `25.5`,
+        x:`0.25mol`
+    },
+    {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    }, {
+        id: 1,
+        task: ``,
+        desc:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        x:``
+    },
 ];
--- src/pages/task/task1.js
+++ src/pages/task/task1.js
@@ -1,62 +1,250 @@
 import * as React from "react";
 import {useState} from "react";
 import {motion,AnimatePresence} from "framer-motion";
+import './kletka.css'
+import parse from 'html-react-parser';
+
 export const Task1 = () => {
+    const parse = require('html-react-parser');
+    let i=0;
+    let text1='25 gr natriy oksidinin’(Na <sub>2</sub>O) mug’darin tabin’?';
+
     const [isVisible, setVisible] = useState(false);
     const [isVisible2, setVisible2] = useState(false);
-    const handleVisibality = () =>{ setVisible(!isVisible)}
-    const handleVisibality2 = () =>{ setVisible2(!isVisible)}
-    setTimeout(handleVisibality,0);
-    setTimeout(handleVisibality2,0);
-    return (<div className="container-xl flex flex w-[100%] h-[100vh] justify-center items-center">
-                <div className="flex flex-col items-center">
-                    <p className="m-[50px] text-3xl">25 gr natriy oksidinin’  (Na<sub>2</sub>O)  mug’darin tabin’?</p>
-                  <div className="flex flex-row">
-                      <div className="flex justify-center flex-wrap items-center rounded-lg shadow-inner bg-slate-50 w-[500px] h-[350px] p-[20px] m-[20px]">
-                          <p className="text-2xl ">
-                              Berilgen: Mr(Na<sub>2</sub>O) =2*23=16=62/mol
-                          </p>
-                      </div>
-                      <div className="flex flex-col justify-center items-center rounded-lg shadow-inner bg-slate-50 w-[500px] h-[350px] p-[20px] m-[20px]">
+    const [isVisible3, setVisible3] = useState(false);
+    const [isVisible4, setVisible4] = useState(false);
+    const [isVisible5, setVisible5] = useState(false);
+    const [isVisible6, setVisible6] = useState(false);
+    const [isVisible7, setVisible7] = useState(false);
+    const [isVisible8, setVisible8] = useState(0);
+    const [isVisible9, setVisible9] = useState(0);
+    const handleVisibality = () =>{ setVisible(true)}
+    const handleVisibality2 = () =>{ setVisible2(true)}
+    const handleVisibality3 = () =>{ setVisible3(true)}
+    const handleVisibality4 = () =>{ setVisible4(true)}
+    const handleVisibality5 = () =>{ setVisible5(true)}
+    const handleVisibality6 = () =>{ setVisible6(true)}
+    const handleVisibality7 = () =>{ setVisible7(true)}
+    const handleVisibality8 = () =>{ setVisible8(1)}
+    const handleVisibality9 = () =>{ setVisible8(0); setVisible9(2)}
+    setTimeout(handleVisibality,3000);
+    setTimeout(handleVisibality2,5000);
+    setTimeout(handleVisibality3,6000);
+    setTimeout(handleVisibality4,9000);
+    setTimeout(handleVisibality5,12000);
+    setTimeout(handleVisibality6,15000);
+    setTimeout(handleVisibality7,18000);
+    setTimeout(handleVisibality8,21000);
+    setTimeout(handleVisibality9,90000);
+
+
+
+    return (<div className="container-xl flex flex w-[100%] h-[100vh] justify-center items-center kletka">
+                <div className="flex flex-col items-center ">
+                    <motion.div
+                        initial={{marginBottom:'25px', opacity: 0, x: '-100vh' }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 4 }}
+
+                    >
+                        <p className="italic text-purple-800 text-3xl">
+                            {parse(text1)}
+                        </p>
+                    </motion.div>
+                           {
+                            isVisible &&
+                            <motion.div
+                                initial={{ opacity: 0, x: '-100vh'}}
+                                animate={{ opacity: 1, x: 0 }}
+                                transition={{ duration: 3 }}
+
+                            >
+                                <p className="italic text-rose-600 text-3xl">
+                                    Berilgen: Mr(Na<sub>2</sub>O) =2*23=16=62/mol
+                                </p>
+                            </motion.div>
+                        }
+
+
+
+
+                      <div className="flex flex-col justify-center items-center rounded-lg shadow-inner  w-[500px] h-[350px] p-[20px] m-[20px]">
                           <div className="flex flex-row justify-center items-center mb-[50px]">
-                              <div className="flex flex-col ml-[50px] h-[120px] w-[50px]  justify-between">
-                                  <AnimatePresence>
-                                      {
-                                          isVisible && (<motion.p
-                                              initial={{opacity:0}}
-                                              animate={{opacity:1}}
-                                              transition={{duration:3}}
-                                              className="text-2xl ">
-                                              160
-                                          </motion.p>)
-                                      }
-                                  </AnimatePresence>
+                              <div className="flex-col justify-center items-center">
+                                  <div className="h-[100px] w-[80px] flex justify-center items-center">
+                                      <AnimatePresence>
+                                          {
+                                              isVisible2 && (<motion.div
+                                                  initial={{opacity:0}}
+                                                  animate={{opacity:1}}
+                                                  transition={{duration:3}}
+                                                  >
+                                              <p className="italic text-purple-800 m-[25px] text-5xl">1mol</p>
 
-                                  <p className="text-2xl">112</p>
-                              </div>
-                              <div className="m-[10px]">
-                                  <AnimatePresence>
-                                      {
-                                          isVisible2 &&(<motion.div
-                                              initial={{height:"5px",width:'0px', opacity:0}}
-                                              animate={{height:'5px',width:'200px',opacity:1}}
-                                              transition={{duration:5}}
-                                              className="bg-[black]"></motion.div>)
-                                      }
-
+                                              </motion.div>)
+                                          }
                                       </AnimatePresence>
-                                  <div className="absolute z-[100]  w-[200px] h-[5px] rotate-[-25deg] bg-[black]"></div>
+
+                                  </div>
+                                  <div className="h-[100px] w-[80px] flex justify-center items-center">
+                                      <AnimatePresence>
+                                          {
+                                              isVisible5 && (<motion.div
+                                                  initial={{opacity:0}}
+                                                  animate={{opacity:1}}
+                                                  transition={{duration:3}}
+                                              >
+                                                  <p className="italic text-purple-800 m-[25px] text-5xl">X</p>
+
+                                              </motion.div>)
+                                          }
+                                      </AnimatePresence>
+
+                                  </div>
                               </div>
-                              <div className="flex flex-col mr-[50px] h-[120px] w-[50px] justify-between items-center">
-                                  <p className="text-2xl">100%</p>
-                                  <p className="text-2xl">X</p>
+                              <div className="flex flex-col justify-center items-center ">
+                                  <div className="h-[100px] w-[250px] flex justify-center items-center">
+                                      <AnimatePresence>
+                                          {
+                                              isVisible3 && (<motion.div
+                                                  initial={{opacity:0,width:0}}
+                                                  animate={{opacity:1,width:"200px"}}
+                                                  transition={{duration:3}}
+                                                  className="h-[5px] bg-black"
+                                              >
+                                              </motion.div>)
+                                          }
+                                      </AnimatePresence>
+                                  </div>
+                                  <div className="h-[100px] w-[250px] flex justify-center items-center">
+                                      <AnimatePresence>
+                                          {
+                                              isVisible6 && (<motion.div
+                                                  initial={{opacity:0,width:0}}
+                                                  animate={{opacity:1,width:"200px"}}
+                                                  transition={{duration:3}}
+                                                  className="h-[5px] bg-black"
+                                              >
+                                              </motion.div>)
+                                          }
+                                      </AnimatePresence>
+                                  </div>
+                              </div>
+                              <div className="flex-col justify-center items-center">
+                                  <div className="h-[100px] w-[80px] flex justify-center items-center">
+                                      <AnimatePresence>
+                                          {
+                                              isVisible4 && (<motion.div
+                                                  initial={{opacity:0}}
+                                                  animate={{opacity:1}}
+                                                  transition={{duration:3}}
+                                              >
+                                                  <p className="italic text-purple-800 m-[25px] text-5xl">62gr</p>
+
+                                              </motion.div>)
+                                          }
+                                      </AnimatePresence>
+
+                                  </div>
+                                  <div className="h-[100px] w-[80px] flex justify-center items-center">
+                                      <AnimatePresence>
+                                          {
+                                              isVisible7 && (<motion.div
+                                                  initial={{opacity:0}}
+                                                  animate={{opacity:1}}
+                                                  transition={{duration:3}}
+                                              >
+                                                  <p className="italic text-purple-800 m-[25px] text-5xl">25</p>
+
+                                              </motion.div>)
+                                          }
+                                      </AnimatePresence>
+                                  </div>
                               </div>
                           </div>
-                          <p className="text-3xl animate-bounce text-bold">X=70%</p>
+                          <AnimatePresence>
+                              {
+                                  isVisible8===1 && (<motion.div
+                                      initial={{opacity:0}}
+                                      animate={{opacity:1,
+                                      transitionEnd:{
+                                          display:'block',
+                                          opacity:1
+                                      }}}
+                                      transition={{duration:5}}
+                                  >
+                                      {isVisible9!==2 && <p className="italic text-purple-800 text-5xl  text-bold">X=?</p>}
+                                      {isVisible9===2 &&
+                                          <motion.p
+                                              initial={{opacity:0}}
+                                              animate={{opacity:1,
+                                                  transitionEnd:{
+                                                  display:"block"
+                                                  }}}
+                                              transition={{duration:3}}
+                                              exit={{opacity:1}}
+                                          className="italic text-rose-500 text-5xl text-bold">X=0.4mol
+                                          </motion.p>}
+
+                                  </motion.div>)
+
+                              }
+                          </AnimatePresence>
+                          {/*<AnimatePresence>
+                              {
+                                  isVisible9===2 && (<motion.div
+                                      initial={{opacity:0}}
+                                      animate={{opacity:1}}
+                                      transition={{duration:3}}
+
+                                  >
+                                      <p className="italic text-purple-800 text-3xl animate-bounce text-bold">X=25</p>
+
+                                  </motion.div>)
+                              }
+                          </AnimatePresence>*/}
+
                       </div>
-                  </div>
+
 
                 </div>
 
             </div>)
 }
+
+
+/*
+<div className="flex flex-row justify-center items-center mb-[50px]">
+    <div className="flex flex-col ml-[50px] h-[120px] w-[50px]  justify-between">
+        <AnimatePresence>
+            {
+                isVisible && (<motion.p
+                    initial={{opacity:0}}
+                    animate={{opacity:1}}
+                    transition={{duration:3}}
+                    className="text-2xl ">
+                    160
+                </motion.p>)
+            }
+        </AnimatePresence>
+
+        <p className="italic text-purple-800 text-2xl">112</p>
+    </div>
+    <div className="m-[10px]">
+        <AnimatePresence>
+            {
+                isVisible2 &&(<motion.div
+                    initial={{height:"5px",width:'0px', opacity:0}}
+                    animate={{height:'5px',width:'200px',opacity:1}}
+                    transition={{duration:5}}
+                    className="bg-[black]"></motion.div>)
+            }
+
+        </AnimatePresence>
+        <div className="absolute z-[100]  w-[200px] h-[5px] rotate-[-25deg] bg-[black]"></div>
+    </div>
+    <div className="flex flex-col mr-[50px] h-[120px] w-[50px] justify-between items-center">
+        <p className="italic text-purple-800 text-2xl">100%</p>
+        <p className="italic text-purple-800 text-2xl">X</p>
+    </div>
+</div>*/
