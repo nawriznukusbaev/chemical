127e14af45edff1e16c13b6beb0e4a523b2e10df
Nawriz Nukusbaev <39462907+nawriznukusbaev@users.noreply.github.com>
2026-03-30 14:51:22 +0500

    Update task1.js

--- src/pages/task/task1/task1.js
+++ src/pages/task/task1/task1.js
@@ -14,8 +14,6 @@ export const TaskOneTask = () => {
     const navigate=useNavigate();
     const goBack=()=>navigate(-1);
 
-    const parse = require('html-react-parser');
-    let i = 0;
 
     const [isVisible, setVisible] = useState(false);
     const [isVisible2, setVisible2] = useState(false);
@@ -29,8 +27,6 @@ export const TaskOneTask = () => {
     const [isVisible9, setVisible9] = useState(false);
     const [isVisible10, setVisible10] = useState(false);
     const [isVisible11, setVisible11] = useState(false);
-    const [isVisible12, setVisible12] = useState(false);
-    const [isVisible13, setVisible13] = useState(false);
 
 
     if(data.id===6 || data.id===7 || data.id===9){
